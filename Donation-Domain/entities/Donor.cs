366f2e408ec6b9f44dbfa831a97e31f7a1e32a13
eduardo04alv <eduardo04guz@icloud.com>
2025-04-16 23:31:18 -0400

    .

--- Donation-Domain/entities/Donor.cs
+++ Donation-Domain/entities/Donor.cs
@@ -8,6 +8,8 @@ namespace Donation_Domain.entities
     {
         [Key]
         public int id_donor { get; set; }
+
+
         public string name_donor { get; set; }
         public string email_donor { get; set; }
         public string phone_donor { get; set; }
